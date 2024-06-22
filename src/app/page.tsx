908089bd7659d5283dd2438e957007cab17b4841
panyongxu1002 <panyx1002@gmail.com>
2024-06-23 05:29:18 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,23 @@ export default function Home() {
   return (
     <div className="flex justify-center align-center mt-[200px] p-[20px]" >
       <AuthButton challenge={challenge}></AuthButton>
+      {/* {authResult && <div>Auth Result: {JSON.stringify(authResult, null, 2)}</div>} */}
+
+
+      {/* <div className="flex justify-center items-center px-3 py-2.5 text-center rounded-lg border border-solid border-zinc-300 md:w-[579px] max-md:px-5">
+        <div className="flex gap-3">
+          <div className="flex flex-col justify-center text-3xl font-medium leading-5 text-white whitespace-nowrap">
+            <div className="justify-center items-center leading-[3.25rem] bg-black rounded-2xl h-[52px] w-[52px]">
+              OP
+            </div>
+          </div>
+          <div className="my-auto text-2xl leading-7 text-zinc-900">
+            Sign in with OpenAccount
+          </div>
+        </div>
+      </div> */}
+
+
     </div>
   );
 }
