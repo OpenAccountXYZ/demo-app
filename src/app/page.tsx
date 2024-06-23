0784080f457c02fd4518d1bc24e86b8fc8f76d4c
suneal <xurigoong@gmail.com>
2024-06-23 23:10:48 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
   }
 
   useEffect(() => {
-    if (authResult) {
+    if (authResult && (authResult as any).fullChallenge) {
       // got user signature, handle user login
       message.success("Got user auth signature. Please veryify.")
       let fullChallenge: any = (authResult as any).fullChallenge;
