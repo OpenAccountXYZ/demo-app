5542979e878ca3ae43aca0fb1770dbe14697101a
panyongxu1002 <panyx1002@gmail.com>
2024-06-23 13:00:59 +0800

    add new feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 
 import React, { useEffect, useState } from 'react';
 import { AuthButton, useAuthWindow, verifySignature } from "openaccount-connect";
-import {message} from 'antd';
-import {Textarea} from "@nextui-org/react";
-import {Button} from "@nextui-org/react";
+import { message } from 'antd';
+import { Textarea } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
 
@@ -14,6 +14,8 @@ export default function Home() {
   // get challenge from server
   // const challenge = await fetch("/api/challenge");
   const challenge = crypto.getRandomValues(new Uint8Array(32)).toString();
+
+  const [userInfo, setUserInfo] = useState("");
 
 
   // Define an internal async function
@@ -41,30 +43,45 @@ export default function Home() {
     if (authResult) {
       // got user signature, handle user login
       message.success("Got user auth signature. Please veryify.")
+      let fullChallenge: any = (authResult as any).fullChallenge;
+      // `Address: ${fullChallenge?.account}\nChainID:${fullChallenge.chainID}`
+      setUserInfo(`Address: ${fullChallenge?.account}\nChainID:${fullChallenge.chainID}`)
     }
   }, [authResult]);
 
 
   return (
-    <div className="justify-center align-center mt-[200px] p-[20px]" >
+    <div className="justify-center align-center mt-[20%] p-[20px]" >
       <p className="text-4xl font-bold text-center">OpenAccount Connect Demo App</p>
-      <AuthButton challenge={challenge}></AuthButton>
-      <Textarea
-        isReadOnly
-        label="User Info"
-        variant="bordered"
-        value={authResult ? `Address: ${authResult.fullChallenge.account}\nChainID:${authResult.fullChallenge.chainID}` : ""}
-        className="max-w"/>
-      <Textarea
-        isReadOnly
-        label="Auth Result"
-        variant="bordered"
-        value={authResult ? JSON.stringify(authResult, null, 2) : ""}
-        className="max-w"/>
+      <div className="flex justify-center align-center mt-[20px] mb-[20px]">
+        <AuthButton challenge={challenge}></AuthButton>
+      </div>
+      {
+        authResult && <Textarea
+          isReadOnly
+          label="User Info"
+          variant="bordered"
+          value={userInfo}
+          className="max-w mb-[20px]" />
+      }
+      {
+        authResult && <Textarea
+          isReadOnly
+          label="Auth Result"
+          variant="bordered"
+          value={JSON.stringify(authResult, null, 2)}
+          className="max-w" />
+      }
 
-      <Button color="primary" isDisabled={authResult === null} onClick={onPress}>
-        Verify Auth Signature With EIP1271
-      </Button>
+      <div className="flex justify-center align-center mt-[20px] mb-[20px]">
+
+        <Button color="primary"
+          isDisabled={authResult === null}
+          onClick={onPress}
+        >
+          Verify Auth Signature With EIP1271
+        </Button>
+      </div>
     </div>
   );
 }
