3bc0a82e0d737d4f27ba677efe78343a158436f7
suneal <xurigoong@gmail.com>
2024-06-24 21:03:59 +0800

    update style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
   return (
     <div className="justify-center align-center mt-[100px] p-[20px]" >
       <p className="text-4xl font-bold text-center mb-[100px]">OpenAccount Demo App</p>
-      <div className="flex justify-center align-center mt-[20px] mb-[40px]">
+      <div className="flex justify-center align-center mt-[00px] mb-[80px]">
         <AuthButton challenge={challenge}></AuthButton>
 
       </div>
